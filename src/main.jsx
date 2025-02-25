0974314a995f1a4bb4918430c200a06852a2ff61
kyl6682 <kyl6682@gmail.com>
2025-02-25 14:54:02 +0900

    main.jsx에 CartProvidor 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { MenuProvider } from "./context/menuContext.jsx";
+import { CartProvidor } from "./context/cartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <MenuProvider>
-      <App />
+      <CartProvidor>
+        <App />
+      </CartProvidor>
     </MenuProvider>
   </BrowserRouter>
 );
