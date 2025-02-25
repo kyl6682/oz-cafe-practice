8f8441e82c8fa1f8e2630fc2cdf9b679c5ec9861
kyl6682 <kyl6682@gmail.com>
2025-02-25 14:42:22 +0900

    main.jsx에 MenuProvidor 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { MenuProvider } from "./context/menuContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <MenuProvider>
+      <App />
+    </MenuProvider>
   </BrowserRouter>
 );
